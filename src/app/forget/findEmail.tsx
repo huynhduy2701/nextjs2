814c3ef7ad6e38f79562627b42144f10cfc1a137
huynhduy <nlhd.2701@gmail.com>
2024-05-06 22:06:13 +0700

    ca

--- src/app/forget/findEmail.tsx
+++ src/app/forget/findEmail.tsx
@@ -1,37 +1,84 @@
 "use client"
-import { useState, MouseEvent, ChangeEvent, useEffect } from 'react';
+import { useState, MouseEvent, ChangeEvent, useEffect, useRef } from 'react';
 import './stylle.scss'
-import { useRouter } from 'next/router'
 import { showErrorToast, showSuccessToast } from '../erros/erros'
+import { ConfirmationResult,getAuth,signInWithPhoneNumber, } from 'firebase/auth';
+import {app} from './firebase'
+import { useRouter } from 'next/router';
 interface UserData{
     email :string,
-    numberPhone:string
+    phone:string
 }
 const FindEmail=()=>{
 
     const [email,setEmail]= useState("")
     const [numberPhone,setNumberPhone]=useState('')
     const [getValueForm,setGetValueForm]=useState('')
+    const [otp,setOtp]=useState('')
+    const [otpSent,setOtpSent]=useState(false)
+    const recaptchaVerifierRef = useRef<RecaptchaVerifier | null>(null);
+    const [confirmationResult, setConfirmationResult] = useState<ConfirmationResult | undefined>(undefined);
+    const auth = getAuth(app)
+    const router = useRouter()
     const handleGetValue = (e: ChangeEvent<HTMLInputElement>) => {
         setGetValueForm(e.target.value);
     };
+    useEffect(() => {
+        recaptchaVerifierRef.current = new RecaptchaVerifier(auth, 'recaptcha-container', {
+            'size': 'normal',
+            'callback': () => {
+                // Your callback logic here
+            }
+        }) 
+    }, [auth])
     useEffect(()=>{
         const listUser = localStorage.getItem('listUser') 
-    })
-    const handleFindEmail= async(e:MouseEvent<HTMLButtonElement>)=>{
-        console.log("So dien thoai : ",getValueForm)
-        e.preventDefault();
-         // Kiểm tra xem giá trị đầu vào chỉ chứa các chữ số hay không
+            // Kiểm tra xem giá trị đầu vào chỉ chứa các chữ số hay không
     if (/^\d+$/.test(getValueForm)) {
         // Nếu chỉ chứa các chữ số, loại bỏ các số 0 ở đầu và thêm +84
-        const phoneNumber = getValueForm.replace(/^0+/, '+84');
+        // const phoneNumber = getValueForm.replace(/^0+/, '+84');
+        const phoneNumber = getValueForm;
+
         setNumberPhone(phoneNumber);
-        console.log("email : ", numberPhone);
+        console.log("số điện thoại : ", numberPhone);
     } else {
         // Nếu chứa kí tự không phải chữ số, coi nó là địa chỉ email
         setEmail(getValueForm);
         console.log("email : ", email);
     }
+    })
+    const handleOTPChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setOtp(e.target.value)
+    }
+    const handleSendOTP = async () => {
+        const formatPhone= numberPhone.replace(/^0+/, '+84');
+
+        try {
+            const formattedPhoneNumber = `+${formatPhone.replace(/\D/g, '')}`
+            const confirmation = await signInWithPhoneNumber(auth, formattedPhoneNumber, recaptchaVerifierRef.current!)
+            setConfirmationResult(confirmation)
+            setOtpSent(true)
+            setNumberPhone('')
+            alert("OTP sent")
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    const handleOTPSubmit = async () => {
+        try {
+            if (confirmationResult) {
+                await confirmationResult.confirm(otp)
+                setOtp('')
+                router.push('/dashboard')
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    const handleFindEmail= async(e:MouseEvent<HTMLButtonElement>)=>{
+        // console.log("So dien thoai : ",getValueForm)
+        e.preventDefault();
+     
        
         const listUser = localStorage.getItem('listUser')
         const newListUser: UserData[] = listUser ? JSON.parse(listUser) : [];
@@ -54,24 +101,24 @@ const FindEmail=()=>{
         else if (newListUser.length>0) {
             let isChecked= false;
             newListUser.forEach((e: UserData) => {
-                if (e.email === email) {
+                if (e.email === email || e.phone === numberPhone) {
                     isChecked = true;
-                    console.log("Có tài khoản người dùng");
+                    console.log("Tìm thấy tài khoản người dùng");
                 }
             });
             if(!isChecked){
-                console.log("Nhập sai email");
-                showErrorToast("Nhập sai email")
+                console.log("Nhập sai email hoặc số điện thoai");
+                showErrorToast("Nhập sai email hoặc số điện thoai")
                 return
             }else{
                 console.log("tìm thấy người dùng");
                     const userData: UserData = {
                         email: email ,
-                        numberPhone:numberPhone
+                        phone:numberPhone
                     };
                     
                     // Đẩy user lên localStorage với tên 'mailUser'
-                    localStorage.setItem('mailUser', JSON.stringify(userData));
+                    localStorage.setItem('infoInputUser', JSON.stringify(userData));
                     
                     // Redirect tới trang nhập mã code
                     // router.push("/forget/inputCode");
--- src/app/forget/firebase.js
+++ src/app/forget/firebase.js
@@ -1,6 +1,11 @@
-import firebase from "firebase/compat/app";
-import 'firebase/compat/auth';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBgvvQMDBd5V5BuHmBpfSNlvogoUQApvHM",
   authDomain: "otpnextjs.firebaseapp.com",
@@ -11,6 +16,6 @@ const firebaseConfig = {
   measurementId: "G-DS4RCWFEKB"
 };
 
-const app = firebase.initializeApp(firebaseConfig);
-export const auth = firebase.auth(app);
-export default app;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export {app}
